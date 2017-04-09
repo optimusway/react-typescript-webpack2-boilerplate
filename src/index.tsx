37c4a02c346f6f83e30749038e9eb5ddd51e3f86
Aleksandr Skakovskiy <optimusway@gmail.com>
2017-04-09 05:31:11 +0500

    add container component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { Counter } from './components/Counter/index';
+import { App } from './containers/App/index';
 
 render(
-  <Counter />,
-  document.body,
+  <App>
+    <Counter />
+  </App>,
+  document.getElementById('root'),
 );
